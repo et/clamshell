1a61225a11306da767d43742273e4737cc2bf94f
et <eric.l.m.thomas@gmail.com>
2011-01-28 03:30:36 -0500

    Remove project class

--- lib/spider.rb
+++ lib/spider.rb
@@ -2,7 +2,6 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 require File.dirname(__FILE__) + '/spider/dsl'
-require File.dirname(__FILE__) + '/spider/project'
 require File.dirname(__FILE__) + '/spider/dependency'
 require File.dirname(__FILE__) + '/spider/settings'
 require File.dirname(__FILE__) + '/spider/ui'

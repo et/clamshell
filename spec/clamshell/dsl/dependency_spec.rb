7e71c9432df93ad8c0d06f151c8ae56f91b4fada
et <eric.l.m.thomas@gmail.com>
2011-01-30 06:42:07 -0500

    Package git repository so it can versioned and tested from someone else's clone. Basically it extracts an archive to /tmp before running the git tests and then cleans it up afterwards.

--- spec/clamshell/dsl/dependency_spec.rb
+++ spec/clamshell/dsl/dependency_spec.rb
@@ -1,16 +1,31 @@
 require 'spec_helper'
 
 describe Clamshell::Git do
-  before :all do
-    @git = Clamshell::Git.new(FIXTURES_DIR + '/repo/.git',
-                              :ref => '4d16bcb15e413c65e49921837da5527a277e47ee')
 
-    @bad_git = Clamshell::Git.new(FIXTURES_DIR + '/repo/.git',
+  GIT_REPO_PATH = File.join('/tmp', 'git_repo')
+
+  before :all do
+    if !File.directory?(GIT_REPO_PATH)
+      puts "Creating git repository in #{GIT_REPO_PATH}"
+      cmd = "tar -xzvf #{FIXTURES_DIR}/git_repo.tar.gz -C /tmp"
+      IO.popen(cmd) do |pipe|
+        puts pipe.read
+      end
+
+      if !File.directory?(GIT_REPO_PATH)
+        raise "Unable to create testing repository"
+      end
+    end
+
+    @git = Clamshell::Git.new(GIT_REPO_PATH + '/.git',
+                              :ref => '81586f244689250938e20aea135e8f699300feb9')
+
+    @bad_git = Clamshell::Git.new(GIT_REPO_PATH + '/.git',
                                   :ref => '123abc')
   end
 
   it "should return the repository's name" do
-    @git.name.should == 'repo'
+    @git.name.should == 'git_repo'
   end
 
   describe "validate" do
@@ -21,5 +36,10 @@ describe Clamshell::Git do
     it "should not validate" do
       @git.validate.should == true
     end
+  end
+
+  after :all do
+    puts "Removing git repository"
+    FileUtils::Verbose.rm_rf(GIT_REPO_PATH)
   end
 end

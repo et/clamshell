fd6cf58dc22a0819f7b3a69cc816e5a06105d48c
eric <eric.thomas@faa.gov>
2011-01-31 16:31:59 -0500

    Be more specific in how many Git objects.

--- spec/clamshell/dsl/project_spec.rb
+++ spec/clamshell/dsl/project_spec.rb
@@ -14,7 +14,7 @@ describe Clamshell::Project do
   describe "git" do
     it "should create a Git dependency" do
       @project.git("/path/to/git/repo.git", :ref => "12345")
-      @project.instance_variable_get(:@dependencies).any? do |d|
+      @project.instance_variable_get(:@dependencies).one? do |d|
         d.class == Clamshell::Git
       end.should == true
     end
